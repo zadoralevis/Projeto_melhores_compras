31744e400c5db5212adde872c36666116ee50372
zadoralevis <isadoralevislopes@gmail.com>
2025-03-15 19:00:50 -0300

    Add files via upload

---
+++ Arquivo Codigo_Nivel_Atendimento_SAC_Melhores_Compras.py
@@ -0,0 +1,15 @@
+ def classificar_satisfacao(nota):
+ if nota > 90:
+        return "Atendimento de qualidade"
+    if 70 <= nota <= 89:
+        return "Atendimento neutro"
+    else:
+        return "Atendimento insatisfatório"
+ 
+# Entrada da nota de satisfação
+nota_cliente = float(input("Informe a nota de satisfação (0 a 100): "))
+ 
+# Chamada da função e impressão do resultado
+resultado = classificar_satisfacao(nota_cliente)
+print(resultado)
+
